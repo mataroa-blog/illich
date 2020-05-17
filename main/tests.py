78f6efda0a5b65ca19a8bb0a6fc556af373e3225
Theodore Keloglou <zf@sirodoht.com>
2020-05-17 11:36:31 +0100

    add index test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexTestCase(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
