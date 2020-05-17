dc9e1ce98311455c04bb112c504398391b49187d
Theodore Keloglou <zf@sirodoht.com>
2020-05-17 11:00:46 +0100

    black

--- illich/wsgi.py
+++ illich/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'illich.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "illich.settings")
 
 application = get_wsgi_application()
