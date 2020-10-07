b47e74bc58151ae302706cf055c44ceabf199bf0
Theodore Keloglou <zf@sirodoht.com>
2020-10-07 22:24:33 +0100

    add tests for metadata endpoint

--- main/tests.py
+++ main/tests.py
@@ -38,3 +38,27 @@ class BlogCreateDuplicateTestCase(TestCase):
         response = self.client.post(reverse("blog_create"), data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(models.Blog.objects.filter(url=data["url"]).count(), 1)
+
+
+class MetadataTestCase(TestCase):
+    def test_metadata_title(self):
+        data = {
+            "url": "https://sirodoht.com/",
+        }
+        response = self.client.post(reverse("metadata"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "sirodoht blog")
+
+    def test_metadata_invalid_url(self):
+        data = {
+            "url": "this-is not a _URL_",
+        }
+        response = self.client.post(reverse("metadata"), data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_metadata_not_found_url(self):
+        data = {
+            "url": "https://sirodoht.com/404",
+        }
+        response = self.client.post(reverse("metadata"), data)
+        self.assertEqual(response.status_code, 200)
