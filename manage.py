b49f8bdbba8278779b79b6f59bbce00020e34e97
Theodore Keloglou <zf@sirodoht.com>
2021-04-27 18:39:00 +0100

    change dotenv with envrc

--- manage.py
+++ manage.py
@@ -3,12 +3,9 @@
 import os
 import sys
 
-import dotenv
-
 
 def main():
-    dotenv.read_dotenv()
-
+    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "illich.settings")
     try:
         from django.core.management import execute_from_command_line
